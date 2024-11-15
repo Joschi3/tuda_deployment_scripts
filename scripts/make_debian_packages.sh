1913f2b537bd44f758bee7536a4c8a687af80753
Aljoscha Schmidt <aljoscha97@web.de>
2024-11-15 12:50:52 +0100

    Save Colcon log

--- scripts/make_debian_packages.sh
+++ scripts/make_debian_packages.sh
@@ -323,10 +323,10 @@ done
 # Check if filtered arguments are provided #TODO:  --install-base "/opt/${ROSWSS_PROJECT_NAME}"
 if [ ${#FILTERED_ARGS[@]} -gt 0 ]; then
     info "Building specified packages: ${FILTERED_ARGS[@]}"
-    colcon build --base-paths "$ROSWSS_ROOT" --build-base "${DEB_BUILD_PATH}" --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo --packages-up-to "${FILTERED_ARGS[@]}" || exit 1
+    colcon build --base-paths "$ROSWSS_ROOT" --build-base "${DEB_BUILD_PATH}" --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo --packages-up-to "${FILTERED_ARGS[@]}" 2>&1 | tee "${LOG_FOLDER}/colcon.log" || exit 1
 else
     info "Building all packages in the workspace."
-    colcon build --base-paths "$ROSWSS_ROOT" --build-base "${DEB_BUILD_PATH}" --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo || exit 1
+    colcon build --base-paths "$ROSWSS_ROOT" --build-base "${DEB_BUILD_PATH}" --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo 2>&1 | tee "${LOG_FOLDER}/colcon.log" || exit 1
 fi
 
 info "Start building deb packages with timestamp $BUILD_TIMESTAMP"
